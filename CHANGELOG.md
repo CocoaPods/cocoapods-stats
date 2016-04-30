ed22720c0228a8cbb3f3d57216a883d32eba32c2
Samuel Giddins <segiddins@segiddins.me>
2016-04-29 21:38:35 -0500

    Release 1.0.0.rc.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # CocoaPods Stats CHANGELOG
 
-## Master
+## 1.0.0.rc.1 (2016-04-30)
 
 ##### Enhancements
 
--- lib/cocoapods_stats/gem_version.rb
+++ lib/cocoapods_stats/gem_version.rb
@@ -1,3 +1,3 @@
 module CocoapodsStats
-  VERSION = '1.0.0.beta.4'.freeze
+  VERSION = '1.0.0.rc.1'.freeze
 end
